ef2321198480165608b1fc39caef9788444b5279
Vishan Seru <vishanseru9@gmail.com>
2021-01-01 12:58:15 +0530

    react-native screens and navigation library had been removed from project. So it's reference from App.js has been removed now as well.

--- App.js
+++ App.js
@@ -1,27 +1,16 @@
 import React from 'react';
 import {GalioProvider} from 'galio-framework';
 import {materialTheme} from './app/constants/';
-import {enableScreens} from 'react-native-screens';
-import {NavigationContainer} from '@react-navigation/native';
 import Root from "./app/components/screens/Root";
 import store from "./app/redux/Store";
-import { Provider } from "react-redux";
-
-/*
-Why we need to call this here has been explained below:
-
-https://github.com/software-mansion/react-native-screens#usage-with-react-navigation
-*/
-enableScreens();
+import {Provider} from "react-redux";
 
 export default function App() {
   return (
     <Provider store={store}>
-      <NavigationContainer>
-        <GalioProvider theme={materialTheme}>
-          <Root/>
-        </GalioProvider>
-      </NavigationContainer>
+      <GalioProvider theme={materialTheme}>
+        <Root/>
+      </GalioProvider>
     </Provider>
   );
 }
