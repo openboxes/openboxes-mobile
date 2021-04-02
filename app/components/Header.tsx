52e8e769d46f1c13e280eb93a94df8e7e39e6c87
Vishan Seru <vishanseru9@gmail.com>
2021-04-02 10:32:56 +0530

    Header can be turned into a functional component.

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,48 +1,14 @@
 import React from "react";
-import {ViewProps} from "react-native";
-import {AppState} from "../redux/Reducer";
-import {connect} from "react-redux";
 import {Appbar} from "react-native-paper";
 
-export interface OwnProps extends ViewProps {
+export interface Props {
   title: string
 }
 
-interface StateProps {
-
+export default function Header(props: Props) {
+  return (
+    <Appbar.Header>
+      <Appbar.Content title={props.title}/>
+    </Appbar.Header>
+  )
 }
-
-interface DispatchProps {
-
-}
-
-type Props = OwnProps & StateProps & DispatchProps
-
-interface State {
-
-}
-
-class Header extends React.Component<Props, State> {
-
-  constructor(props: Props) {
-    super(props)
-  }
-
-  render() {
-    return (
-      <Appbar.Header>
-        <Appbar.Content title={this.props.title}/>
-      </Appbar.Header>
-    )
-  }
-}
-
-const mapStateToProps = (state: AppState): StateProps => ({
-  //no-op
-});
-
-const mapDispatchToProps: DispatchProps = {
-  //no-op
-};
-
-export default connect<StateProps, DispatchProps, OwnProps, AppState>(mapStateToProps, mapDispatchToProps)(Header)
