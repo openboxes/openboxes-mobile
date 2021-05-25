d006b25f984e91862dfb67b8159f962fb64274bd
Vishan Seru <vishanseru9@gmail.com>
2021-05-25 17:02:05 +0530

    LoggedInHome scope components is unnecessary. Dashboard component has been promoted up the hierarchy instead.

--- app/components/screens/Dashboard.tsx
+++ app/components/screens/Dashboard.tsx
@@ -5,9 +5,10 @@ import {AppState} from "../../redux/Reducer";
 import {connect} from "react-redux";
 import Icon, {Name} from "../Icon";
 import Theme from "../../utils/Theme";
+import Products from "./products/Products";
 
 export interface OwnProps {
-  onProductsCardPressed: () => void
+
 }
 
 interface StateProps {
@@ -20,27 +21,71 @@ interface DispatchProps {
 
 type Props = OwnProps & StateProps & DispatchProps;
 
-interface State {
+enum NavigationState {
+  Here,
+  ProductsScreen
+}
 
+interface State {
+  navigationState: NavigationState
 }
 
 class Dashboard extends React.Component<Props, State> {
 
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      navigationState: NavigationState.Here
+    }
+    this.renderContent = this.renderContent.bind(this)
+    this.goToProductsScreen = this.goToProductsScreen.bind(this)
+    this.renderProductsScreen = this.renderProductsScreen.bind(this)
+    this.showDashboardContent = this.showDashboardContent.bind(this)
+  }
+
   render() {
+    switch (this.state.navigationState) {
+      case NavigationState.Here:
+        return this.renderContent()
+      case NavigationState.ProductsScreen:
+        return this.renderProductsScreen()
+    }
+  }
+
+  renderContent() {
     return (
       <View style={styles.screenContainer}>
         <Header
           title="Dashboard"
           backButtonVisible={false}
         />
-        <TouchableOpacity style={styles.countContainer} onPress={this.props.onProductsCardPressed}>
+        <TouchableOpacity
+          style={styles.countContainer}
+          onPress={this.goToProductsScreen}
+        >
           <View style={styles.countLabelAndIconContainer}>
             <Icon name={Name.Boxes} size={14}/>
             <Text style={styles.countLabel}>Products</Text>
           </View>
         </TouchableOpacity>
       </View>
-    );
+    )
+  }
+
+  goToProductsScreen() {
+    this.setState({
+      navigationState: NavigationState.ProductsScreen
+    })
+  }
+
+  renderProductsScreen() {
+    return (<Products exit={this.showDashboardContent}/>)
+  }
+
+  showDashboardContent() {
+    this.setState({
+      navigationState: NavigationState.Here
+    })
   }
 }
 
--- app/components/screens/Root.tsx
+++ app/components/screens/Root.tsx
@@ -9,7 +9,7 @@ import {StyleSheet, View} from "react-native";
 import {Session} from "../../data/auth/Session";
 import getSession from "../../data/auth/GetSession";
 import showPopup from "../Popup";
-import LoggedInHome from "../scope/LoggedInHome";
+import Dashboard from "./Dashboard";
 
 export interface OwnProps {
   //no-op
@@ -74,7 +74,7 @@ class Root extends React.Component<Props, State> {
     if (this.props.loggedIn) {
       if (this.props.currentLocation != null) {
         if(this.props.session != null) {
-          content = <LoggedInHome/>
+          content = <Dashboard/>
         } else {
           content = null
         }
