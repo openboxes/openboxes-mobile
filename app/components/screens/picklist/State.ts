238d89e0d89d54d001e5c99a5b777f079b3490c0
chetan <chetan.maharishi@gmail.com>
2021-10-01 02:36:22 +0530

    OBKN-54 Allow a warehouse user to scan a SKU and bin location to pick an item for an outbound order

--- app/components/screens/picklist/State.ts
+++ app/components/screens/picklist/State.ts
@@ -1,9 +1,16 @@
 import PickList from "../../../data/picklist/PickList";
 import Item from "../../../data/picklist/Item";
 import Order from "../../../data/order/Order";
+import Product from "../../../data/product/Product";
+import BinLocation from "../../../data/picklist/BinLocation";
 
 export interface State {
   pickListItem: Item | null
   error: string | null
   order: Order | null
+  productSearchQuery: string
+  binLocationSearchQuery: string
+  quantityPicked: string
+  product: Product | null
+  binLocation: BinLocation | null
 }
