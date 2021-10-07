2253f1be124bcb7b8a9787406b5b10be63e1ef4c
chetan <chetan.maharishi@gmail.com>
2021-10-07 22:11:09 +0530

    Search orders by order name. if one order found then redirect to order detail page

--- app/data/order/GetOrders.ts
+++ app/data/order/GetOrders.ts
@@ -10,7 +10,7 @@ interface GetOrdersApiResponse {// StockMovement Object
 export default function getOrders(query: string | null): Promise<Order[]> {
   let getUrl = url
   if(query!=null){
-    getUrl += "&orderNumber="+query
+    getUrl += "&name="+query
   }
   return apiClient.get(getUrl)
     .then((response: GetOrdersApiResponse) => response.data)
