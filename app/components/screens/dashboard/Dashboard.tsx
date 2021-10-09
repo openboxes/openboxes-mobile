4cf6d69d4666357c9ccbca000972469da380e180
jmiranda <jcm62@columbia.edu>
2021-10-08 23:09:31 -0500

    OBKN-157 Implemented datawedge sample code to play around with broadcast intents that are passed through datawedge API

--- app/components/screens/dashboard/Dashboard.tsx
+++ app/components/screens/dashboard/Dashboard.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import {StyleSheet, Text, TouchableOpacity, View} from "react-native";
+import {Platform, StyleSheet, Text, TouchableOpacity, View, ScrollView, FlatList, TouchableHighlight, Alert} from "react-native";
+import {CheckBox, Button} from 'react-native-elements'
 import Header from "../../Header";
 import {AppState} from "../../../redux/Reducer";
 import {connect} from "react-redux";
@@ -10,13 +11,29 @@ import {DispatchProps, OwnProps, Props, StateProps} from "./Props";
 import {NavigationState, State} from "./State";
 import ScreenContainer from "../../ScreenContainer";
 import Orders from "../orders/Orders";
+import { DeviceEventEmitter } from 'react-native';
+import DataWedgeIntents from 'react-native-datawedge-intents'
 
 class Dashboard extends React.Component<Props, State> {
 
   constructor(props: Props) {
     super(props);
+    console.log("constructor");
     this.state = {
-      navigationState: NavigationState.Here
+      navigationState: NavigationState.Here,
+      ean8checked: true,
+      ean13checked: true,
+      code39checked: true,
+      code128checked: true,
+      lastApiVisible: false,
+      lastApiText: "Messages from DataWedge will go here",
+      checkBoxesDisabled: true,
+      scanButtonVisible: false,
+      dwVersionText: "Pre 6.3.  Please create and configure profile manually.  See the ReadMe for more details",
+      dwVersionTextStyle: styles.itemTextAttention,
+      activeProfileText: "Requires DataWedge 6.3+",
+      enumeratedScannersText: "Requires DataWedge 6.3+",
+      scans: [],
     }
     this.renderContent = this.renderContent.bind(this)
     this.goToProductsScreen = this.goToProductsScreen.bind(this)
@@ -24,7 +41,148 @@ class Dashboard extends React.Component<Props, State> {
     this.renderProductsScreen = this.renderProductsScreen.bind(this)
     this.renderOrdersScreen = this.renderOrdersScreen.bind(this)
     this.showDashboardContent = this.showDashboardContent.bind(this)
+    this.sendCommandResult = "false";
   }
+
+  componentDidMount() {
+    console.log("componentDidMount");
+      this.state.deviceEmitterSubscription = DeviceEventEmitter.addListener('datawedge_broadcast_intent', (intent) => {this.broadcastReceiver(intent)});
+      this.registerBroadcastReceiver();
+      this.determineVersion();
+  }
+
+  componentWillUnmount() {
+    //this.state.deviceEmitterSubscription.remove();
+  }
+
+  _onPressScanButton() {
+    this.sendCommand("com.symbol.datawedge.api.SOFT_SCAN_TRIGGER", 'TOGGLE_SCANNING');
+  }
+
+  determineVersion() {
+    this.sendCommand("com.symbol.datawedge.api.GET_VERSION_INFO", "");
+  }
+
+  setDecoders() {
+    //  Set the new configuration
+    var profileConfig = {
+        "PROFILE_NAME": "ZebraReactNativeDemo",
+        "PROFILE_ENABLED": "true",
+        "CONFIG_MODE": "UPDATE",
+        "PLUGIN_CONFIG": {
+            "PLUGIN_NAME": "BARCODE",
+            "PARAM_LIST": {
+                //"current-device-id": this.selectedScannerId,
+                "scanner_selection": "auto",
+                "decoder_ean8": "" + this.state.ean8checked,
+                "decoder_ean13": "" + this.state.ean13checked,
+                "decoder_code128": "" + this.state.code128checked,
+                "decoder_code39": "" + this.state.code39checked
+            }
+        }
+    };
+    this.sendCommand("com.symbol.datawedge.api.SET_CONFIG", profileConfig);
+  }
+
+  sendCommand(extraName, extraValue) {
+    console.log("Sending Command: " + extraName + ", " + JSON.stringify(extraValue));
+    var broadcastExtras = {};
+    broadcastExtras[extraName] = extraValue;
+    broadcastExtras["SEND_RESULT"] = this.sendCommandResult;
+    DataWedgeIntents.sendBroadcastWithExtras({
+        action: "com.symbol.datawedge.api.ACTION",
+        extras: broadcastExtras});
+  }
+
+  registerBroadcastReceiver() {
+    console.log("registerBroadcastReceiver");
+    DataWedgeIntents.registerBroadcastReceiver({
+      filterActions: [
+          'com.zebra.reactnativedemo.ACTION',
+          'com.symbol.datawedge.api.RESULT_ACTION'
+      ],
+      filterCategories: [
+          'android.intent.category.DEFAULT'
+      ]
+    });
+  }
+
+  broadcastReceiver(intent)
+  {
+    alert("test");
+    //  Broadcast received
+    console.log('Received Intent: ' + JSON.stringify(intent));
+    if (intent.hasOwnProperty('RESULT_INFO')) {
+        var commandResult = intent.RESULT + " (" +
+            intent.COMMAND.substring(intent.COMMAND.lastIndexOf('.') + 1, intent.COMMAND.length) + ")";// + JSON.stringify(intent.RESULT_INFO);
+        this.commandReceived(commandResult.toLowerCase());
+    }
+
+    if (intent.hasOwnProperty('com.symbol.datawedge.api.RESULT_GET_VERSION_INFO')) {
+        //  The version has been returned (DW 6.3 or higher).  Includes the DW version along with other subsystem versions e.g MX
+        var versionInfo = intent['com.symbol.datawedge.api.RESULT_GET_VERSION_INFO'];
+        console.log('Version Info: ' + JSON.stringify(versionInfo));
+        var datawedgeVersion = versionInfo['DATAWEDGE'];
+        console.log("Datawedge version: " + datawedgeVersion);
+
+        //  Fire events sequentially so the application can gracefully degrade the functionality available on earlier DW versions
+//         if (datawedgeVersion >= "06.3")
+//             this.datawedge63();
+//         if (datawedgeVersion >= "06.4")
+//             this.datawedge64();
+//         if (datawedgeVersion >= "06.5")
+//             this.datawedge65();
+
+        this.setState(this.state);
+    }
+    else if (intent.hasOwnProperty('com.symbol.datawedge.api.RESULT_ENUMERATE_SCANNERS')) {
+        //  Return from our request to enumerate the available scanners
+        var enumeratedScannersObj = intent['com.symbol.datawedge.api.RESULT_ENUMERATE_SCANNERS'];
+        this.enumerateScanners(enumeratedScannersObj);
+    }
+    else if (intent.hasOwnProperty('com.symbol.datawedge.api.RESULT_GET_ACTIVE_PROFILE')) {
+        //  Return from our request to obtain the active profile
+        var activeProfileObj = intent['com.symbol.datawedge.api.RESULT_GET_ACTIVE_PROFILE'];
+        this.activeProfile(activeProfileObj);
+    }
+    else if (!intent.hasOwnProperty('RESULT_INFO')) {
+        //  A barcode has been scanned
+        this.barcodeScanned(intent, new Date().toLocaleString());
+    }
+  }
+
+  commandReceived(commandText) {
+    this.state.lastApiText = commandText;
+    this.setState(this.state);
+  }
+
+  enumerateScanners(enumeratedScanners) {
+    var humanReadableScannerList = "";
+    for (var i = 0; i < enumeratedScanners.length; i++)
+    {
+        console.log("Scanner found: name= " + enumeratedScanners[i].SCANNER_NAME + ", id=" + enumeratedScanners[i].SCANNER_INDEX + ", connected=" + enumeratedScanners[i].SCANNER_CONNECTION_STATE);
+        humanReadableScannerList += enumeratedScanners[i].SCANNER_NAME;
+        if (i < enumeratedScanners.length - 1)
+            humanReadableScannerList += ", ";
+    }
+    this.state.enumeratedScannersText = humanReadableScannerList;
+  }
+
+  activeProfile(theActiveProfile) {
+    this.state.activeProfileText = theActiveProfile;
+    this.setState(this.state);
+  }
+
+  barcodeScanned(scanData, timeOfScan) {
+    var scannedData = scanData["com.symbol.datawedge.data_string"];
+    var scannedType = scanData["com.symbol.datawedge.label_type"];
+    console.log("Scan: " + scannedData);
+    this.state.scans.unshift({ data: scannedData, decoder: scannedType, timeAtDecode: timeOfScan });
+    console.log(this.state.scans);
+    this.setState(this.state);
+  }
+
+
 
   render() {
     switch (this.state.navigationState) {
@@ -62,6 +220,34 @@ class Dashboard extends React.Component<Props, State> {
             <Text style={styles.countLabel}>Orders</Text>
           </View>
         </TouchableOpacity>
+        <View style={styles.container}>
+          <Text style={styles.itemHeading}>Scanned barcodes will be displayed here:</Text>
+
+          <FlatList
+            data={this.state.scans}
+            extraData={this.state}
+            keyExtractor={item => item.timeAtDecode}
+            renderItem={({item, separators}) => (
+              <TouchableHighlight
+              onShowUnderlay={separators.highlight}
+              onHideUnderlay={separators.unhighlight}>
+              <View style={{
+                backgroundColor: '#0077A0',
+                margin:10,
+                borderRadius: 5,
+              }}>
+              <View style={{flexDirection: 'row', flex: 1}}>
+              <Text style={styles.scanDataHead}>{item.decoder}</Text>
+              <View style={{flex: 1}}>
+                <Text style={styles.scanDataHeadRight}>{item.timeAtDecode}</Text>
+              </View>
+              </View>
+              <Text style={styles.scanData}>{item.data}</Text>
+              </View>
+            </TouchableHighlight>
+            )}
+          />
+        </View>
       </ScreenContainer>
     )
   }
