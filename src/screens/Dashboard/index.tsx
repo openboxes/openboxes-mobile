7d20a3cbb26d6abcf786c5bf96b0dfb99d186da4
Kumaran <kumaran@softsuave.com>
2021-11-03 23:26:55 +0530

    Bug fixes in dashboard

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -78,7 +78,9 @@ class Dashboard extends React.Component<Props, State> {
                         <Text style={styles.countLabel}>Create LPN</Text>
                     </View>
                 </TouchableOpacity>
-                <TouchableOpacity>
+                <TouchableOpacity onPress={() => {
+                    this.props.navigation.navigate('InternalTransfer');
+                }}>
                     <View style={styles.countLabelAndIconContainer}>
                         <Text style={styles.countLabel}> Internal Transfer</Text>
                     </View>
