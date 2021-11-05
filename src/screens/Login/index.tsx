65c080b3eefe03ca7d7630a7f85df17d7a77f1d8
brilik <megabrilik@gmail.com>
2021-11-05 14:45:24 -0400

    add Choose Url button in login screen

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -6,6 +6,7 @@ import {login} from '../../redux/actions/auth';
 import showPopup from '../../components/Popup';
 import {TextInput} from 'react-native-paper';
 import Button from '../../components/Button';
+import * as NavigationService from "../../NavigationService";
 import {RootState} from '../../redux/reducers';
 
 import {StackNavigationProp} from '@react-navigation/stack';
@@ -18,45 +19,45 @@ import EYE_HIDE from '../../assets/images/eye_hide.png'
 type ProfileScreenNavigationProp = StackNavigationProp<StackList, 'Login'>;
 
 interface OwnProps {
-  navigation: ProfileScreenNavigationProp;
+    navigation: ProfileScreenNavigationProp;
 }
 
 interface StateProps {
-  //no-op
+    //no-op
 }
 
 interface DispatchProps {
-  login: (data: any) => void;
+    login: (data: any) => void;
 }
 
 type Props = OwnProps & StateProps & DispatchProps;
 
 interface State {
-  username: string;
-  password: string;
+    username: string;
+    password: string;
 }
 
-const Login =()=>{
-  const dispatch = useDispatch()
-  const [state,setState] = useState<any>( {
-      username: '',
-      password: '',
-      isSeePassword:true,
+const Login = () => {
+    const dispatch = useDispatch()
+    const [state, setState] = useState<any>({
+        username: '',
+        password: '',
+        isSeePassword: true,
     });
 
- const onUsernameChange = (username: string) => {
-   setState({
-      ...state,
-      username: username,
-    });
-  };
+    const onUsernameChange = (username: string) => {
+        setState({
+            ...state,
+            username: username,
+        });
+    };
 
-  const onPasswordChange = (password: string) => {
-    setState({
-      ...state,
-      password: password,
-    });
-  };
+    const onPasswordChange = (password: string) => {
+        setState({
+            ...state,
+            password: password,
+        });
+    };
 
  const onLoginPress = () => {
     const {username, password} = state;
@@ -101,51 +102,58 @@ const Login =()=>{
       showPopup({message: e.message ?? 'Login failed'});
     }
   };
-  const onPasswordClick= () =>{
-    setState({ ...state,isSeePassword: !state.isSeePassword});
+  const onPasswordClick = () => {
+    setState({...state, isSeePassword: !state.isSeePassword});
   }
 
     return (
-      <View style={styles.screenContainer}>
-        <View style={styles.inputContainer}>
-          <TextInput
-            mode="outlined"
-            label={"Username"}
-            placeholder="Username"
-            onChangeText={onUsernameChange}
-          />
-          <TextInput
-            mode="outlined"
-            placeholder="Password"
-            label={"Password"}
-            secureTextEntry={state.isSeePassword}
-            right={
-                  <TextInput.Icon
-                      name={state.isSeePassword ? EYE_SHOW : EYE_HIDE}
-                      onPress={onPasswordClick}
-                  />
-            }
-            onChangeText={onPasswordChange}
-            style={{
-              marginTop: 8,
-            }}
-          />
-          <Button
-            title="Login"
-            onPress={onLoginPress}
-            style={{
-              marginTop: 8,
-            }}
-          />
-          <Button
-            title="Auto Login"
-            onPress={onAutoLoginPress}
-            style={{
-              marginTop: 8,
-            }}
-          />
+        <View style={styles.screenContainer}>
+            <View style={styles.inputContainer}>
+                <TextInput
+                    mode="outlined"
+                    label={"Username"}
+                    placeholder="Username"
+                    onChangeText={onUsernameChange}
+                />
+                <TextInput
+                    mode="outlined"
+                    placeholder="Password"
+                    label={"Password"}
+                    secureTextEntry={state.isSeePassword}
+                    right={
+                        <TextInput.Icon
+                            name={state.isSeePassword ? EYE_SHOW : EYE_HIDE}
+                            onPress={onPasswordClick}
+                        />
+                    }
+                    onChangeText={onPasswordChange}
+                    style={{
+                        marginTop: 8,
+                    }}
+                />
+                <Button
+                    title="Login"
+                    onPress={onLoginPress}
+                    style={{
+                        marginTop: 8,
+                    }}
+                />
+                <Button
+                    title="Choose Url"
+                    onPress={()=>{NavigationService.navigate("Settings")}}
+                    style={{
+                        marginTop: 8,
+                    }}
+                />
+                <Button
+                     title="Auto Login"
+                     onPress={onAutoLoginPress}
+                     style={{
+                         marginTop: 8,
+                     }}
+                />
+            </View>
         </View>
-      </View>
     );
 
 }
