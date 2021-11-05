869a6a12a4594f3b7ee20ecd755caf981c39d73c
brilik <megabrilik@gmail.com>
2021-11-05 14:45:24 -0400

    add Choose Url button in login screen

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -6,6 +6,7 @@ import {login} from '../../redux/actions/auth';
 import showPopup from '../../components/Popup';
 import {TextInput} from 'react-native-paper';
 import Button from '../../components/Button';
+import * as NavigationService from "../../NavigationService";
 import {RootState} from '../../redux/reducers';
 
 import {StackNavigationProp} from '@react-navigation/stack';
@@ -18,123 +19,130 @@ import EYE_HIDE from '../../assets/images/eye_hide.png'
 type ProfileScreenNavigationProp = StackNavigationProp<StackList, 'Login'>;
 
 interface OwnProps {
-  navigation: ProfileScreenNavigationProp;
+    navigation: ProfileScreenNavigationProp;
 }
 
 interface StateProps {
-  //no-op
+    //no-op
 }
 
 interface DispatchProps {
-  login: (data: any) => void;
+    login: (data: any) => void;
 }
 
 type Props = OwnProps & StateProps & DispatchProps;
 
 interface State {
-  username: string;
-  password: string;
+    username: string;
+    password: string;
 }
 
-const Login =()=>{
-  const dispatch = useDispatch()
-  const [state,setState] = useState<any>( {
-      username: '',
-      password: '',
-      isSeePassword:true,
+const Login = () => {
+    const dispatch = useDispatch()
+    const [state, setState] = useState<any>({
+        username: '',
+        password: '',
+        isSeePassword: true,
     });
 
- const onUsernameChange = (username: string) => {
-   setState({
-      ...state,
-      username: username,
-    });
-  };
+    const onUsernameChange = (username: string) => {
+        setState({
+            ...state,
+            username: username,
+        });
+    };
 
-  const onPasswordChange = (password: string) => {
-    setState({
-      ...state,
-      password: password,
-    });
-  };
+    const onPasswordChange = (password: string) => {
+        setState({
+            ...state,
+            password: password,
+        });
+    };
 
- const onLoginPress = () => {
-    const {username, password} = state;
-    const loginDisallowedReason = getLoginDisallowedReason(
-      username,
-      password,
-    );
-    if (loginDisallowedReason) {
-      showPopup({message: loginDisallowedReason, positiveButton: "ok"});
-    } else {
-        onLogin(username, password);
+    const onLoginPress = () => {
+        const {username, password} = state;
+        const loginDisallowedReason = getLoginDisallowedReason(
+            username,
+            password,
+        );
+        if (loginDisallowedReason) {
+            showPopup({message: loginDisallowedReason, positiveButton: "ok"});
+        } else {
+            onLogin(username, password);
+        }
+    };
+
+    const getLoginDisallowedReason = (
+        username: string,
+        password: string,
+    ): string | null => {
+        if (!username) {
+            return 'Username not provided';
+        }
+
+        if (!password) {
+            return 'Password not provided';
+        }
+
+        if (password.length < 6) {
+            return 'Password is less than 6 characters';
+        }
+
+        return null;
+    };
+
+    const onLogin = (username: string, password: string) => {
+        try {
+            dispatch(login({username, password}));
+        } catch (e) {
+            showPopup({message: e.message ?? 'Login failed'});
+        }
+    };
+    const onPasswordClick = () => {
+        setState({...state, isSeePassword: !state.isSeePassword});
     }
-  };
-
-  const getLoginDisallowedReason = (
-    username: string,
-    password: string,
-  ): string | null => {
-    if (!username) {
-      return 'Username not provided';
-    }
-
-    if (!password) {
-      return 'Password not provided';
-    }
-
-    if (password.length < 6) {
-      return 'Password is less than 6 characters';
-    }
-
-    return null;
-  };
-
-  const onLogin = (username: string, password: string) => {
-    try {
-      dispatch(login({username, password}));
-    } catch (e) {
-      showPopup({message: e.message ?? 'Login failed'});
-    }
-  };
-  const onPasswordClick= () =>{
-    setState({ ...state,isSeePassword: !state.isSeePassword});
-  }
 
     return (
-      <View style={styles.screenContainer}>
-        <View style={styles.inputContainer}>
-          <TextInput
-            mode="outlined"
-            label={"Username"}
-            placeholder="Username"
-            onChangeText={onUsernameChange}
-          />
-          <TextInput
-            mode="outlined"
-            placeholder="Password"
-            label={"Password"}
-            secureTextEntry={state.isSeePassword}
-            right={
-                  <TextInput.Icon
-                      name={state.isSeePassword ? EYE_SHOW : EYE_HIDE}
-                      onPress={onPasswordClick}
-                  />
-            }
-            onChangeText={onPasswordChange}
-            style={{
-              marginTop: 8,
-            }}
-          />
-          <Button
-            title="Login"
-            onPress={onLoginPress}
-            style={{
-              marginTop: 8,
-            }}
-          />
+        <View style={styles.screenContainer}>
+            <View style={styles.inputContainer}>
+                <TextInput
+                    mode="outlined"
+                    label={"Username"}
+                    placeholder="Username"
+                    onChangeText={onUsernameChange}
+                />
+                <TextInput
+                    mode="outlined"
+                    placeholder="Password"
+                    label={"Password"}
+                    secureTextEntry={state.isSeePassword}
+                    right={
+                        <TextInput.Icon
+                            name={state.isSeePassword ? EYE_SHOW : EYE_HIDE}
+                            onPress={onPasswordClick}
+                        />
+                    }
+                    onChangeText={onPasswordChange}
+                    style={{
+                        marginTop: 8,
+                    }}
+                />
+                <Button
+                    title="Login"
+                    onPress={onLoginPress}
+                    style={{
+                        marginTop: 8,
+                    }}
+                />
+                <Button
+                    title="Choose Url"
+                    onPress={()=>{NavigationService.navigate("Settings")}}
+                    style={{
+                        marginTop: 8,
+                    }}
+                />
+            </View>
         </View>
-      </View>
     );
 
 }
