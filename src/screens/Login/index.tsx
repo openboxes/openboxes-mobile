ad3b41232de606e8de2ffbc82e0d9c12479f88d3
brilik <megabrilik@gmail.com>
2021-11-05 17:21:04 -0400

    change text

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -135,7 +135,7 @@ const Login = () => {
                     }}
                 />
                 <Button
-                    title="Choose Url"
+                    title="Settings"
                     onPress={()=>{NavigationService.navigate("Settings")}}
                     style={{
                         marginTop: 8,
