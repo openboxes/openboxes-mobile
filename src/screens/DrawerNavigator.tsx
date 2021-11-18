55ce97e0c38554ef70016ea4a64dddfbae0a53f9
hellochirag <chiragjadav134@gmail.com>
2021-11-18 09:27:39 +0530

    [Fix] OBKN-243
    App build on bitrise is missing logo and icons

--- src/screens/DrawerNavigator.tsx
+++ src/screens/DrawerNavigator.tsx
@@ -1,36 +1,47 @@
+/* eslint-disable react-native/no-inline-styles */
 import React, {Component} from 'react';
 import {createDrawerNavigator} from '@react-navigation/drawer';
-import ChooseCurrentLocation from "./ChooseCurrentLocation";
-import {Image} from 'react-native'
-import {colors} from "../constants";
-import InternalTransfer from "./InternalTransfer";
+import ChooseCurrentLocation from './ChooseCurrentLocation';
+import {Image} from 'react-native';
+import {colors} from '../constants';
+import InternalTransfer from './InternalTransfer';
 
 const Drawer = createDrawerNavigator();
 
 class DrawerNavigator extends Component {
-    render() {
-        return (
-            <Drawer.Navigator screenOptions={{
-                headerStyle: {
-                    backgroundColor: colors.headerColor
-                },
-                headerRight: () => <Image
-                    source={require('../assets/images/logo.png')}
-                    style={{resizeMode: 'stretch', width: 40, height: 30, marginRight: 30}}
-                />
-            }}>
-                <Drawer.Screen
-                    name="Choose Current Location"
-                    component={ChooseCurrentLocation}
-                    options={{
-                        // headerStyle: {
-                        //     backgroundColor: '#f4511e',
-                        // }
-                    }}
-                />
-            </Drawer.Navigator>
-        );
-    }
+  render() {
+    return (
+      <Drawer.Navigator
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: colors.headerColor,
+          },
+          headerRight: () => (
+            <Image
+              source={require('../assets/images/logo.png')}
+              style={{
+                resizeMode: 'stretch',
+                width: 40,
+                height: 30,
+                marginRight: 30,
+              }}
+            />
+          ),
+        }}>
+        <Drawer.Screen
+          name="Choose Current Location"
+          component={ChooseCurrentLocation}
+          options={
+            {
+              // headerStyle: {
+              //     backgroundColor: '#f4511e',
+              // }
+            }
+          }
+        />
+      </Drawer.Navigator>
+    );
+  }
 }
 
 export default DrawerNavigator;
