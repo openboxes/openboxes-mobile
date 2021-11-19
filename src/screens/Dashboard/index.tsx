37f7c82e530dfe1f763993851d0b41245ad2f841
Kumaran <kumaran@softsuave.com>
2021-11-20 01:13:47 +0530

    Dashboard changes

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -3,68 +3,11 @@ import {FlatList, Image, ListRenderItemInfo, Text, TouchableOpacity, View} from 
 import styles from './styles';
 import {connect} from 'react-redux';
 import {DispatchProps, Props, State} from './Types';
-
 import {RootState} from '../../redux/reducers';
 import {Card} from "react-native-paper";
+import dashboardData from "./dashboardData";
 
-const Dashboard_Data = [
-    {
-        screenName: "Picking",
-        icon: require("../../assets/images/picking.png"),
-        navigationScreenName: "Orders"
-    },
-    {
-        screenName: "Packing",
-        icon: require("../../assets/images/packing.png"),
-        navigationScreenName: "OutboundStockList"
-    },
-    {
-        screenName: "Loading",
-        icon: require("../../assets/images/loading.png"),
-        navigationScreenName: "PutawayCandidates"
-    },
-    {
-        screenName: "Transfer",
-        icon: require("../../assets/images/transfer.png"),
-        navigationScreenName: "InternalTransfer"
-    },
-    {
-        screenName: "Inventory",
-        icon: require("../../assets/images/inventory.png"),
-        navigationScreenName: "Product Summary"
-    },
-    {
-        screenName: "Lookup",
-        icon: require("../../assets/images/lookup.png"),
-        navigationScreenName: "CreateLpn"
-    },
-    {
-        screenName: "Receiving",
-        icon: require("../../assets/images/receiving.png"),
-        navigationScreenName: "InboundOrderList"
-    },
-    {
-        screenName: "Put Away",
-        icon: require("../../assets/images/putaway.png"),
-        navigationScreenName: "PutawayList"
-    },
-    {
-        screenName: "Scan",
-        icon: require("../../assets/images/scan.jpg"),
-        navigationScreenName: "Scan"
-    },
 
-    {
-        screenName: "Product",
-        icon: require("../../assets/images/product.png"),
-        navigationScreenName: "Products"
-    },
-    {
-        screenName: "Put Away Candidates",
-        icon: require("../../assets/images/putaway.png"),
-        navigationScreenName: "PutawayCandidates"
-    },
-]
 
 class Dashboard extends React.Component<Props, State> {
 
@@ -88,7 +31,7 @@ class Dashboard extends React.Component<Props, State> {
         return (
             <View style={styles.screenContainer}>
                 <FlatList
-                    data={Dashboard_Data}
+                    data={dashboardData}
                     horizontal={false}
                     numColumns={3}
                     renderItem={(item: ListRenderItemInfo<any>) => this.renderItem(item.item, item.index)}
