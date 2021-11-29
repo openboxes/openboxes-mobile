a452de84ecfd0ca37a6d7736cdf43cab2843a95e
hellochirag <chiragjadav134@gmail.com>
2021-11-29 22:18:48 +0530

    Fix OBKN-319
    Add create LPN button to OutboundStockDetails

--- src/screens/Dashboard/dashboardData.ts
+++ src/screens/Dashboard/dashboardData.ts
@@ -1,60 +1,60 @@
 const dashboardData = [
-    {
-        screenName: "Picking",
-        icon: require("../../assets/images/picking.png"),
-        navigationScreenName: "Orders"
-    },
-    {
-        screenName: "Packing",
-        icon: require("../../assets/images/packing.png"),
-        navigationScreenName: "OutboundStockList"
-    },
-    {
-        screenName: "Loading",
-        icon: require("../../assets/images/loading.png"),
-        navigationScreenName: "PutawayCandidates"
-    },
-    {
-        screenName: "Transfer",
-        icon: require("../../assets/images/transfer.png"),
-        navigationScreenName: "InternalTransfer"
-    },
-    {
-        screenName: "Inventory",
-        icon: require("../../assets/images/inventory.png"),
-        navigationScreenName: "Product Summary"
-    },
-    {
-        screenName: "Lookup",
-        icon: require("../../assets/images/lookup.png"),
-        navigationScreenName: "CreateLpn"
-    },
-    {
-        screenName: "Receiving",
-        icon: require("../../assets/images/receiving.png"),
-        navigationScreenName: "InboundOrderList"
-    },
-    {
-        screenName: "Put Away",
-        icon: require("../../assets/images/putaway.png"),
-        navigationScreenName: "PutawayList"
-    },
-    {
-        screenName: "Scan",
-        icon: require("../../assets/images/scan.jpg"),
-        navigationScreenName: "Scan"
-    },
+  {
+    screenName: 'Picking',
+    icon: require('../../assets/images/picking.png'),
+    navigationScreenName: 'Orders',
+  },
+  {
+    screenName: 'Packing',
+    icon: require('../../assets/images/packing.png'),
+    navigationScreenName: 'OutboundStockList',
+  },
+  {
+    screenName: 'Loading',
+    icon: require('../../assets/images/loading.png'),
+    navigationScreenName: 'PutawayCandidates',
+  },
+  {
+    screenName: 'Transfer',
+    icon: require('../../assets/images/transfer.png'),
+    navigationScreenName: 'InternalTransfer',
+  },
+  {
+    screenName: 'Inventory',
+    icon: require('../../assets/images/inventory.png'),
+    navigationScreenName: 'Product Summary',
+  },
+  {
+    screenName: 'Lookup',
+    icon: require('../../assets/images/lookup.png'),
+    navigationScreenName: 'CreateLpn',
+  },
+  {
+    screenName: 'Receiving',
+    icon: require('../../assets/images/receiving.png'),
+    navigationScreenName: 'InboundOrderList',
+  },
+  {
+    screenName: 'Put Away',
+    icon: require('../../assets/images/putaway.png'),
+    navigationScreenName: 'PutawayList',
+  },
+  {
+    screenName: 'Scan',
+    icon: require('../../assets/images/scan.jpg'),
+    navigationScreenName: 'Scan',
+  },
 
-    {
-        screenName: "Product",
-        icon: require("../../assets/images/product.png"),
-        navigationScreenName: "Products"
-    },
-    {
-        screenName: "Put Away Candidates",
-        icon: require("../../assets/images/putaway.png"),
-        navigationScreenName: "PutawayCandidates"
-    },
-]
+  {
+    screenName: 'Product',
+    icon: require('../../assets/images/product.png'),
+    navigationScreenName: 'Products',
+  },
+  {
+    screenName: 'Put Away Candidates',
+    icon: require('../../assets/images/putaway.png'),
+    navigationScreenName: 'PutawayCandidates',
+  },
+];
 
-export default dashboardData
+export default dashboardData;
