8ba76540e6228f8574ad6d64653ae1e969d8454b
hellochirag <chiragjadav134@gmail.com>
2021-11-30 17:23:14 +0530

    Fix OBKN-320
    Fix Allow user to navigate to LPN details from OutboundStockDetails

--- src/components/OptionMenu.tsx
+++ src/components/OptionMenu.tsx
@@ -1,60 +1,68 @@
 import * as React from 'react';
 import {Image, TouchableOpacity, View} from 'react-native';
 import {Menu, Provider} from 'react-native-paper';
-import {useDispatch} from "react-redux";
-import {logout} from "../redux/actions/auth";
+import {useDispatch} from 'react-redux';
+import {logout} from '../redux/actions/auth';
 
 const OptionMenu = ({route}: any) => {
-    const dispatch = useDispatch()
-    const [visible, setVisible] = React.useState(false);
+  const dispatch = useDispatch();
+  const [visible, setVisible] = React.useState(false);
 
-    const openMenu = () => setVisible(true);
+  const openMenu = () => setVisible(true);
 
-    const closeMenu = () => setVisible(false);
-    const handleLogout = () => dispatch(logout())
-    return (
-        <Provider>
-            <View
-                style={{
-                    flexDirection: 'row',
-                    justifyContent: 'center',
-                }}>
+  const closeMenu = () => setVisible(false);
+  const handleLogout = () => dispatch(logout());
+  return (
+    <Provider>
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'center',
+        }}>
+        <Image
+          source={require('../assets/images/logo.png')}
+          style={{
+            resizeMode: 'stretch',
+            width: 40,
+            height: 30,
+            marginTop: 10,
+            marginEnd:
+              route.name !== 'Login' && route.name !== 'Settings' ? 0 : 30,
+          }}
+        />
+        {route.name !== 'Login' && route.name !== 'Settings' ? (
+          <Menu
+            style={{
+              width: 350,
+              marginTop: 30,
+            }}
+            visible={visible}
+            onDismiss={closeMenu}
+            anchor={
+              <TouchableOpacity onPress={openMenu}>
                 <Image
-                    source={require('../assets/images/logo.png')}
-                    style={{
-                        resizeMode: 'stretch',
-                        width: 40,
-                        height: 30,
-                        marginTop: 10,
-                        marginEnd: route.name !== 'Login' && route.name !== 'Settings' ? 0 : 30
-                    }}
+                  source={require('../assets/images/option.jpg')}
+                  style={{
+                    resizeMode: 'stretch',
+                    width: 40,
+                    height: 40,
+                    marginRight: 10,
+                  }}
                 />
-                {route.name !== 'Login' && route.name !== 'Settings' ? <Menu
-                    style={{
-                        width: 350,
-                        marginTop: 30,
-                    }}
-                    visible={visible}
-                    onDismiss={closeMenu}
-                    anchor={<TouchableOpacity
-                        onPress={openMenu}>
-                        <Image
-                            source={require('../assets/images/option.jpg')}
-                            style={{
-                                resizeMode: 'stretch',
-                                width: 40,
-                                height: 40,
-                                marginRight: 10,
-                            }}
-                        />
-                    </TouchableOpacity>}>
-                    <Menu.Item style={{
-                        width: "70%",
-                    }} onPress={handleLogout} title="Logout"/>
-                </Menu> : null}
-            </View>
-        </Provider>
-    );
+              </TouchableOpacity>
+            }>
+            <Menu.Item
+              style={{
+                width: '70%',
+              }}
+              onPress={handleLogout}
+              title="Logout"
+            />
+          </Menu>
+        ) : null}
+      </View>
+    </Provider>
+  );
 };
 
 export default OptionMenu;
