d03a3029fee2937758f78c12a93f17360b3192ec
hellochirag <chiragjadav134@gmail.com>
2021-11-30 19:04:04 +0530

    Fix OBKN-359
    It's difficult to scroll up putaway candidates list because of refresh

--- src/screens/PutawayCandidates/styles.ts
+++ src/screens/PutawayCandidates/styles.ts
@@ -1,9 +1,13 @@
-import {StyleSheet} from "react-native";
+import {StyleSheet} from 'react-native';
 
 export default StyleSheet.create({
-    itemBox: {
-        borderWidth: 1,
-        margin: 5,
-        padding: 5
-    }
-})
+  itemBox: {
+    borderWidth: 1,
+    margin: 5,
+    padding: 5,
+  },
+  container: {
+    flex: 1,
+    paddingVertical: 10,
+  },
+});
