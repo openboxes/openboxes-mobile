3eda574051cfe4069c5c14d97f28638cbeeee5c1
Kumaran <kumaran@softsuave.com>
2021-12-01 23:37:17 +0530

    Bug fixes

--- src/screens/Lpn/Create.tsx
+++ src/screens/Lpn/Create.tsx
@@ -1,216 +1,201 @@
-import React, {ReactElement, useState} from 'react';
+import React from 'react';
 import showPopup from '../../components/Popup';
-import {getStockMovements} from '../../redux/actions/orders';
 import {saveAndUpdateLpn} from '../../redux/actions/lpn';
 import {DispatchProps, Props} from './Types';
 import {connect} from 'react-redux';
-import {Image, Text, View} from 'react-native';
+import {Text, View} from 'react-native';
 import {Order} from '../../data/order/Order';
 import styles from './styles';
 import InputBox from '../../components/InputBox';
 import Button from '../../components/Button';
-import SelectDropdown from 'react-native-select-dropdown';
-import {
-  getShipmentPacking,
-  getShipmentOrigin,
-} from '../../redux/actions/packing';
+import {getShipmentOrigin, getShipmentPacking,} from '../../redux/actions/packing';
 import {RootState} from '../../redux/reducers';
-import Location from '../../data/location/Location';
 import AutoInputInternalLocation from '../../components/AutoInputInternalLocation';
 
 export interface State {
-  stockMovements: Order[] | null;
-  open: false;
-  stockMovement: Order | null;
-  name: string | null;
-  containerNumber: string | null;
-  stockMovementList: string[] | [];
-  stockMovementId: string | any;
+    stockMovements: Order[] | null;
+    open: false;
+    stockMovement: Order | null;
+    name: string | null;
+    containerNumber: string | null;
+    stockMovementList: string[] | [];
+    stockMovementId: string | any;
 }
 
 class CreateLpn extends React.Component<Props, State> {
-  constructor(props: Props) {
-    super(props);
-    const {id, shipmentDetail} = this.props.route.params;
-    this.state = {
-      stockMovements: shipmentDetail?.shipmentNumber || null,
-      stockMovement: shipmentDetail?.shipmentNumber || null,
-      stockMovementList: [],
-      open: false,
-      name: null,
-      containerNumber: null,
-      stockMovementId: id || null,
+    constructor(props: Props) {
+        super(props);
+        const {id, shipmentDetail} = this.props?.route?.params ?? '';
+        this.state = {
+            stockMovements: shipmentDetail?.shipmentNumber || null,
+            stockMovement: shipmentDetail?.shipmentNumber || null,
+            stockMovementList: [],
+            open: false,
+            name: null,
+            containerNumber: null,
+            stockMovementId: id || null,
+        };
+    }
+
+    componentDidMount() {
+        this.getShipmentOrigin();
+    }
+
+    fetchContainerList = () => {
+        const actionCallback = (data: any) => {
+            if (data?.error) {
+                showPopup({
+                    title: data.error.message ? 'Container' : null,
+                    message: data.error.message ?? 'Failed to fetch Container List',
+                    positiveButton: {
+                        text: 'Ok',
+                    },
+                });
+            } else {
+                let stockMovementList: string[] = [];
+                // data.map((item: any) => {
+                //     stockMovementList.push(item.name)
+                // })
+                // this.setState({
+                //     stockMovementList: stockMovementList,
+                //     stockMovements: data
+                // })
+            }
+        };
+        this.props.getShipmentPacking('OUTBOUND', actionCallback);
     };
-  }
 
-  componentDidMount() {
-    this.getShipmentOrigin();
-  }
-
-  fetchContainerList = () => {
-    const actionCallback = (data: any) => {
-      if (data?.error) {
-        showPopup({
-          title: data.error.message ? 'Container' : null,
-          message: data.error.message ?? 'Failed to fetch Container List',
-          positiveButton: {
-            text: 'Ok',
-          },
-        });
-      } else {
-        let stockMovementList: string[] = [];
-        // data.map((item: any) => {
-        //     stockMovementList.push(item.name)
-        // })
-        // this.setState({
-        //     stockMovementList: stockMovementList,
-        //     stockMovements: data
-        // })
-      }
+    getShipmentOrigin = () => {
+        const {currentLocation} = this.props;
+        const actionCallback = (data: any) => {
+            if (data?.error) {
+                showPopup({
+                    title: data.error.message ? 'StockMovements' : null,
+                    message: data.error.message ?? 'Failed to fetch StockMovements',
+                    positiveButton: {
+                        text: 'Ok',
+                    },
+                });
+            } else {
+                let stockMovementList: string[] = [];
+                data.map((item: any) => {
+                    stockMovementList.push(item.shipmentNumber);
+                });
+                this.setState({
+                    stockMovementList: stockMovementList,
+                    stockMovements: data,
+                });
+            }
+        };
+        this.props.getShipmentOrigin(currentLocation?.id ?? '', actionCallback);
     };
-    this.props.getShipmentPacking('OUTBOUND', actionCallback);
-  };
 
-  getShipmentOrigin = () => {
-    const {currentLocation} = this.props;
-    const actionCallback = (data: any) => {
-      if (data?.error) {
-        showPopup({
-          title: data.error.message ? 'StockMovements' : null,
-          message: data.error.message ?? 'Failed to fetch StockMovements',
-          positiveButton: {
-            text: 'Ok',
-          },
-        });
-      } else {
-        let stockMovementList: string[] = [];
-        data.map((item: any) => {
-          stockMovementList.push(item.shipmentNumber);
-        });
+    saveLpn = () => {
+        const requestBody = {
+            name: this.state.name,
+            containerNumber: this.state.containerNumber,
+            'containerType.id': '2',
+            'shipment.id': this.state.stockMovementId,
+        };
+        const actionCallback = (data: any) => {
+            if (data?.error) {
+                showPopup({
+                    title: data.error.message ? `Failed to Save` : null,
+                    message: data.error.message ?? `Failed to Save`,
+                    positiveButton: {
+                        text: 'Retry',
+                        callback: () => {
+                            this.props.saveAndUpdateLpn(requestBody, actionCallback);
+                        },
+                    },
+                    negativeButtonText: 'Cancel',
+                });
+            } else {
+                // if (data.length == 0) {
+                //     showPopup({
+                //         message: `No search results`,
+                //         positiveButton: {text: 'Ok'},
+                //     });
+                // } else
+                if (data && Object.keys(data).length !== 0) {
+                    this.props.navigation.navigate('LpnDetail', {
+                        id: data.id,
+                        shipmentNumber: this.state.stockMovement,
+                    });
+                }
+            }
+        };
+        console.debug('Save LPN', requestBody);
+        this.props.saveAndUpdateLpn(requestBody, actionCallback);
+    };
+
+    onChangeName = (text: string) => {
         this.setState({
-          stockMovementList: stockMovementList,
-          stockMovements: data,
+            name: text,
         });
-      }
     };
-    this.props.getShipmentOrigin(currentLocation?.id ?? '', actionCallback);
-  };
 
-  saveLpn = () => {
-    const requestBody = {
-      name: this.state.name,
-      containerNumber: this.state.containerNumber,
-      'containerType.id': '2',
-      'shipment.id': this.state.stockMovementId,
-    };
-    const actionCallback = (data: any) => {
-      if (data?.error) {
-        showPopup({
-          title: data.error.message ? `Failed to Save` : null,
-          message: data.error.message ?? `Failed to Save`,
-          positiveButton: {
-            text: 'Retry',
-            callback: () => {
-              this.props.saveAndUpdateLpn(requestBody, actionCallback);
-            },
-          },
-          negativeButtonText: 'Cancel',
+    onChangeContainerNumber = (text: string) => {
+        this.setState({
+            containerNumber: text,
         });
-      } else {
-        // if (data.length == 0) {
-        //     showPopup({
-        //         message: `No search results`,
-        //         positiveButton: {text: 'Ok'},
-        //     });
-        // } else
-        if (data && Object.keys(data).length !== 0) {
-          this.props.navigation.navigate('LpnDetail', {
-            id: data.id,
-            shipmentNumber: this.state.stockMovement,
-          });
-        }
-      }
     };
-    console.debug('Save LPN', requestBody);
-    this.props.saveAndUpdateLpn(requestBody, actionCallback);
-  };
 
-  onChangeName = (text: string) => {
-    this.setState({
-      name: text,
-    });
-  };
 
-  onChangeContainerNumber = (text: string) => {
-    this.setState({
-      containerNumber: text,
-    });
-  };
-
-  renderIcon = () => {
-    return (
-      <Image
-        style={styles.arrowDownIcon}
-        source={require('../../assets/images/arrow-down.png')}
-      />
-    );
-  };
-
-  render() {
-    console.debug('{item.item.name}:');
-    return (
-      <View style={styles.container}>
-        <View style={styles.from}>
-          <Text style={styles.label}>Shipment Number</Text>
-          <AutoInputInternalLocation
-            label="AutoInputInternalLocation"
-            data={this.state.stockMovementList}
-            selectedData={(selectedItem: any, index: number) => {
-              const stockMovement = this.state.stockMovements?.find(
-                i => i.shipmentNumber === selectedItem,
-              );
-              this.setState({
-                stockMovement: selectedItem,
-                stockMovementId: stockMovement?.id,
-              });
-            }}
-            initValue={this.state.stockMovements}
-          />
-          <InputBox
-            value={this.state.name}
-            onChange={this.onChangeName}
-            editable={false}
-            label={'Name'}
-          />
-          <InputBox
-            value={this.state.containerNumber}
-            onChange={this.onChangeContainerNumber}
-            editable={false}
-            label={'Container Number'}
-          />
-        </View>
-        <View style={styles.bottom}>
-          <Button
-            title="Submit"
-            onPress={this.saveLpn}
-            // eslint-disable-next-line react-native/no-inline-styles
-            style={{
-              marginTop: 8,
-            }}
-          />
-        </View>
-      </View>
-    );
-  }
+    render() {
+        return (
+            <View style={styles.container}>
+                <View style={styles.from}>
+                    <Text style={styles.label}>Shipment Number</Text>
+                    <AutoInputInternalLocation
+                        label="AutoInputInternalLocation"
+                        data={this.state.stockMovementList}
+                        selectedData={(selectedItem: any, index: number) => {
+                            const stockMovement = this.state.stockMovements?.find(
+                                i => i.shipmentNumber === selectedItem,
+                            );
+                            this.setState({
+                                stockMovement: selectedItem,
+                                stockMovementId: stockMovement?.id,
+                            });
+                        }}
+                        initValue={this.state.stockMovements}
+                    />
+                    <InputBox
+                        value={this.state.name}
+                        onChange={this.onChangeName}
+                        editable={false}
+                        label={'Name'}
+                    />
+                    <InputBox
+                        value={this.state.containerNumber}
+                        onChange={this.onChangeContainerNumber}
+                        editable={false}
+                        label={'Container Number'}
+                    />
+                </View>
+                <View style={styles.bottom}>
+                    <Button
+                        title="Submit"
+                        onPress={this.saveLpn}
+                        // eslint-disable-next-line react-native/no-inline-styles
+                        style={{
+                            marginTop: 8,
+                        }}
+                    />
+                </View>
+            </View>
+        );
+    }
 }
 
 const mapStateToProps = (state: RootState) => ({
-  currentLocation: state.mainReducer.currentLocation,
+    currentLocation: state.mainReducer.currentLocation,
 });
 const mapDispatchToProps: DispatchProps = {
-  getShipmentPacking,
-  getShipmentOrigin,
-  saveAndUpdateLpn,
+    getShipmentPacking,
+    getShipmentOrigin,
+    saveAndUpdateLpn,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(CreateLpn);
