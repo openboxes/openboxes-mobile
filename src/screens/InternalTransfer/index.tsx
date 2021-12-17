808ce08a3849b96dcaf11a33c0b1d52dfbd5d4d9
Kumaran <kumaran@softsuave.com>
2021-12-17 23:05:28 +0530

    OBKN-107 Bug fix in Allow a warehouse user to change the status of a container

--- src/screens/InternalTransfer/index.tsx
+++ src/screens/InternalTransfer/index.tsx
@@ -158,7 +158,6 @@ const InternalTransfer = () => {
   };
 
   const onTransfer = () => {
-    // dispatch(showScreenLoading("Update Transfer"))
     const request: any = {
       status: 'COMPLETED',
       stockTransferNumber: '',
@@ -176,33 +175,6 @@ const InternalTransfer = () => {
         },
       ],
     };
-    // const actionCallback = (data: any) => {
-    //     if (data?.error) {
-    //         showPopup({
-    //             title: data.errorMessage
-    //                 ? `Failed to update`
-    //                 : null,
-    //             message:
-    //                 data.errorMessage ??
-    //                 `Failed to update`,
-    //             positiveButton: {
-    //                 text: 'Retry',
-    //                 callback: () => {
-    //                     dispatch(updateStockTransfer(request));
-    //                 },
-    //             },
-    //             negativeButtonText: 'Cancel',
-    //         });
-    //     } else {
-    //         if (data.length == 0) {
-    //             showPopup({
-    //                 message: `No search results`,
-    //                 positiveButton: {text: 'Ok'},
-    //             });
-    //         }
-    //         dispatch(hideScreenLoading());;
-    //     }
-    // };
     dispatch(updateStockTransfer(request));
   };
 
