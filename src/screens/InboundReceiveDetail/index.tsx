5ee6d39b783e570666cf9553940b29c526527260
kumaransoftsuave <50243967+kumaransoftsuave@users.noreply.github.com>
2021-12-17 11:16:35 +0530

    OBKN-377 Should not be able to select shipment from other location on create LPN (#160)
    Co-authored-by: Justin Miranda <jcm62@columbia.edu>

--- src/screens/InboundReceiveDetail/index.tsx
+++ src/screens/InboundReceiveDetail/index.tsx
@@ -20,7 +20,7 @@ const InboundReceiveDetail = () => {
   const [cancelRemaining, setCancelRemaining] = useState(false);
   const navigation = useNavigation();
   const location = useSelector(
-    (state: RootState) => state.mainReducer.currentLocation
+    (state: RootState) => state.locationsReducer.SelectedLocation,
   );
   const [state, setState] = useState<any>({
     comments: '',
