08fdae8ec9259be63f35de3af9f264bda1dcaf45
hellochirag <chiragjadav134@gmail.com>
2021-12-23 12:44:58 +0530

    [Chore] Fixed empty list screen is visible during loading data

---
+++ src/components/EmptyView/types.ts
@@ -0,0 +1,15 @@
+export interface OwnProps {
+  title: string;
+  description?: any;
+  isRefresh: boolean | any;
+  uri?: string | any;
+  onPress?: () => void;
+}
+
+export interface StateProps {
+}
+
+
+export interface DispatchProps {}
+
+export type Props = OwnProps & StateProps & DispatchProps;
