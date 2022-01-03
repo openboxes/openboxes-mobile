32783beeb2fdafdad47ed6ac2707a24e6c2e9a1f
Kumaran <kumaran@softsuave.com>
2022-01-03 23:39:54 +0530

    OBKN-399 Bug fix in Unable to clear selected value of putaway bin

--- src/components/AutoInputBinLocation/index.tsx
+++ src/components/AutoInputBinLocation/index.tsx
@@ -1,11 +1,17 @@
 import React, {useState} from 'react';
 import styles from './styles';
-import {View, TextInput} from 'react-native';
+import {Image, TextInput, TouchableOpacity, View} from 'react-native';
 import {Props} from './types';
 import ModalSelector from 'react-native-modal-selector-searchable';
+import CLEAR from '../../assets/images/icon_clear.png';
 
 export default function ({data, selectedData, initValue = ''}: Props) {
   const [query, setQuery] = useState(initValue);
+
+  const clearSelection = () => {
+    setQuery('');
+    selectedData?.({id: '', name: ''});
+  }
 
   return (
     <View style={styles.mainContainer}>
@@ -21,9 +27,9 @@ export default function ({data, selectedData, initValue = ''}: Props) {
         accessible={true}
         scrollViewAccessibilityLabel={'Scrollable options'}
         cancelButtonAccessibilityLabel={'Cancel Button'}
-        onChange={(option: {label: React.SetStateAction<string>; key: any}) => {
+        onChange={(option: { label: React.SetStateAction<string>; key: any }) => {
           setQuery(option.label);
-          selectedData?.({ id: option.key, name: option.label });
+          selectedData?.({id: option.key, name: option.label});
         }}
       >
         <TextInput
@@ -33,6 +39,10 @@ export default function ({data, selectedData, initValue = ''}: Props) {
           value={query}
         />
       </ModalSelector>
+      {query != '' ?
+        <TouchableOpacity onPress={clearSelection}>
+          <Image source={CLEAR} style={styles.imageIcon} />
+        </TouchableOpacity> : null}
     </View>
   );
 }
