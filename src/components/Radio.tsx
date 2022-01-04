2acbedabc49c0f8012199fd9caa3efd99ff571cf
kumaransoftsuave <50243967+kumaransoftsuave@users.noreply.github.com>
2022-01-04 15:48:38 +0530

    OBKN-422  Add cancel remaining checkbox to receive on mobile app (#180)

--- src/components/Radio.tsx
+++ src/components/Radio.tsx
@@ -2,8 +2,7 @@ import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { RadioButton } from 'react-native-paper';
 
-const Radio = ({ title, check }: any) => {
-  const [checked, setChecked] = useState(check);
+const Radio = ({ title, checked, setChecked, disabled }: any) => {
 
   return (
     <View style={styles.container}>
@@ -11,12 +10,15 @@ const Radio = ({ title, check }: any) => {
         <RadioButton
           color={'grey'}
           value="false"
+          disabled={disabled}
           status={checked ? 'checked' : 'unchecked'}
-          onPress={() => setChecked(!checked)}
+          onPress={() => {
+            setChecked(!checked);
+          }}
         />
       </View>
       <View style={styles.title}>
-        <Text>{title}</Text>
+        <Text style={styles.titleText(disabled)}>{title}</Text>
       </View>
     </View>
   );
@@ -24,14 +26,22 @@ const Radio = ({ title, check }: any) => {
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
+    marginTop: 15,
     flexDirection: 'row',
-    alignItems: 'center'
+    alignItems: 'center',
+    justifyContent: "flex-start"
   },
   title: {
-    flex: 9
+    flex: 9,
   },
   radioButton: {
     flex: 1
-  }
+  },
+  titleText : (disabled : any) =>({
+    fontSize: 16,
+    fontWeight : "bold",
+    color: disabled ? "grey" : "black"
+  }),
 });
 export default Radio;
