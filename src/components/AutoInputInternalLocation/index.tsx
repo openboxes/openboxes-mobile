7bd60ac57ef4c61a7102864bdd61a580414f7ce1
hellochirag <chiragjadav134@gmail.com>
2022-01-13 12:33:51 +0530

    OBKN-340 Error message when try to pack item without selecting container

--- src/components/AutoInputInternalLocation/index.tsx
+++ src/components/AutoInputInternalLocation/index.tsx
@@ -39,7 +39,6 @@ export default function ({
             label: option.label
           });
         }}
-        onCancel={() => setQuery('')}
       >
         <TextInput
           style={styles.textInput}
