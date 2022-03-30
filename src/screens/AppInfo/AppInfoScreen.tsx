e7d267185226064c39236e88c66bfd0fd23c2103
Chirag Jadav <35322132+hellochirag@users.noreply.github.com>
2022-03-30 22:21:04 +0530

    Change app name in Appinfo screen

--- src/screens/AppInfo/AppInfoScreen.tsx
+++ src/screens/AppInfo/AppInfoScreen.tsx
@@ -6,14 +6,14 @@ import DeviceInfo from 'react-native-device-info';
 const AppInfoScreen: FC = () => {
   return (
      <View style={Styles.mainContainer}>
-         <Text style={Styles.openboxTextStyle}>Openbox</Text>
+         <Text style={Styles.openboxTextStyle}>Openboxes</Text>
          <Text style={Styles.versionNumberText}>Version number {DeviceInfo.getVersion()} </Text>
          <Image
             source={require('../../assets/images/logo.png')}
             resizeMode={'cover'}
             style={Styles.imageStyle}
         />
-        <Text style={Styles.certificateText}>2010-2022 Openbox inc.</Text>    
+        <Text style={Styles.certificateText}>2010-2022 Openboxes inc.</Text>    
      </View>
   );
 }
