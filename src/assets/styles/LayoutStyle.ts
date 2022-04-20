fd291555206e1c2901394148bd5c144851cb279b
Roy Zhang <chiragjadav134@gmail.com>
2022-04-20 19:45:32 +0530

    Removed  ```borderRadius: 15``` values

--- src/assets/styles/LayoutStyle.ts
+++ src/assets/styles/LayoutStyle.ts
@@ -1,11 +1,10 @@
 import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
-    listItemContainer: {
-        margin: 8,
-        padding: 5,
-        justifyContent: 'center',
-        borderRadius: 15,
-        elevation: 8,
-      },
+  listItemContainer: {
+    margin: 8,
+    padding: 5,
+    justifyContent: 'center',
+    elevation: 8
+  }
 });
