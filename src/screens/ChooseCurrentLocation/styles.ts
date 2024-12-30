47c1b112781907cd71fa750a8c972fd504e33fc6
olewandowski1 <olewandowski@soldevelo.com>
2024-12-30 15:46:10 +0100

    WMS-30 add info about the number of locations within org

--- src/screens/ChooseCurrentLocation/styles.ts
+++ src/screens/ChooseCurrentLocation/styles.ts
@@ -1,10 +1,25 @@
 import { StyleSheet } from 'react-native';
+import Theme from '../../utils/Theme';
 
 export default StyleSheet.create({
   container: {
     display: 'flex',
-    flexDirection: 'column',
-    flex: 1
+    flexDirection: 'column'
   },
-  scrollView: {}
+  scrollView: {
+    backgroundColor: '#fff',
+    borderRadius: Theme.roundness,
+    marginVertical: 8,
+    marginHorizontal: 8
+  },
+  iconContainer: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+    backgroundColor: Theme.colors.backdrop,
+    marginRight: 8
+  }
 });
