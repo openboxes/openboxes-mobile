887f8ed8f4853c97104ddc15b94c848c2b8c800a
olewandowski1 <olewandowski@soldevelo.com>
2025-10-27 12:44:37 +0100

    OBLS-295 fix screen fragments error

--- android/app/src/main/java/com/openboxes_mobile_o/MainActivity.java
+++ android/app/src/main/java/com/openboxes_mobile_o/MainActivity.java
@@ -1,8 +1,19 @@
 package com.openboxes.android;
 
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 
 public class MainActivity extends ReactActivity {
+
+  // React Native Screens lifecycle fix
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    // ✅ Prevent "Screen fragments should never be restored"
+    if (savedInstanceState != null) {
+      savedInstanceState.remove("android:support:fragments");
+    }
+    super.onCreate(savedInstanceState);
+  }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
