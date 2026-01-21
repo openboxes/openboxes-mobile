bfab0c8603d3eec42bcc73cf74ed0928613c395a
olewandowski1 <olewandowski@soldevelo.com>
2026-01-21 14:31:51 +0100

    OBLS-421 removed redundant styles

--- src/screens/CycleCount/styles.ts
+++ src/screens/CycleCount/styles.ts
@@ -69,94 +69,10 @@ export default StyleSheet.create({
     fontWeight: 'normal'
   },
   topSpace: { marginTop: Theme.spacing.small },
-  bottomSpace: { marginBottom: Theme.spacing.small },
   formContainer: {
     display: 'flex',
     flex: 1,
     flexDirection: 'column',
     padding: Theme.spacing.large
-  },
-  modalOverlay: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    width: '100%',
-    height: '100%',
-    backgroundColor: 'rgba(0, 0, 0, 0.5)'
-  },
-  modalContent: {
-    width: '90%',
-    backgroundColor: 'white',
-    padding: Theme.spacing.large,
-    borderRadius: Theme.roundness * 2
-  },
-  dropdownContainer: {
-    width: '50%',
-    alignSelf: 'flex-end'
-  },
-  bottomActionContainer: {
-    padding: Theme.spacing.small,
-    borderTopColor: Theme.colors.disabled
-  },
-  cardAnnotation: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center'
-  },
-  dialogActions: {
-    flexDirection: 'row',
-    justifyContent: 'flex-end'
-  },
-  dialogNewLocationHeader: {
-    marginBottom: Theme.spacing.large,
-    marginTop: Theme.spacing.small,
-    fontWeight: 'bold',
-    fontSize: 16
-  },
-  dialogCurrentLocationLabel: {
-    fontWeight: 'bold',
-    fontSize: 16
-  },
-  dialogCurrentLocationWrapper: {
-    marginBottom: 16,
-    fontWeight: 'bold'
-  },
-  modalSurface: {
-    width: '90%',
-    borderRadius: 8,
-    padding: 16,
-    elevation: 4,
-    backgroundColor: 'white'
-  },
-  modalHeader: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    marginBottom: 8
-  },
-  modalContainer: {
-    position: 'absolute',
-    top: 0,
-    bottom: 0,
-    left: 0,
-    right: 0
-  },
-  centeredView: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  modalTitleText: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    color: Theme.colors.text,
-    flexShrink: 1
-  },
-  dialogSuggestButton: {
-    marginBottom: Theme.spacing.large,
-    marginTop: Theme.spacing.medium
-  },
-  dialogActionButton: {
-    marginLeft: Theme.spacing.small
   }
 });
