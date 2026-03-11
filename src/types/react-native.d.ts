53c5fe778e316001bd3e52e1e19c769034cbca15
olewandowski1 <olewandowski@soldevelo.com>
2026-03-11 14:36:23 +0100

    OBLS-358 Add comment on react-native.d.ts file

--- src/types/react-native.d.ts
+++ src/types/react-native.d.ts
@@ -1,3 +1,11 @@
+/**
+ * Older versions of @types/react-native don't include hasTVPreferredFocus and tvParallaxProperties
+ * on TouchableWithoutFeedbackProps, but react-native-paper v4 passes these
+ * TV/Apple TV-related props through, causing TypeScript compilation errors.
+ * Without this, some UI primitives that need those props to be passed will fail to compile,
+ * even though the props are mostly relevant for TV platforms.
+ * This file patches the type definitions to suppress those errors.
+ */
 import 'react-native';
 
 declare module 'react-native' {
